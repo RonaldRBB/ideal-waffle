0438247a79e850e4837ccd904c0973067c9f1d06
Ronald Bello <ronaldbello2@gmail.com>
2024-02-12 01:53:49 -0300

    semi funciona

--- src/components/entry/form/index.jsx
+++ src/components/entry/form/index.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
 export default class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            content: ''
+        }
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleTitleChange(event) {
+        this.setState({
+            title: event.target.value
+        });
+    }
+    handleContentChange(event) {
+        this.setState({
+            content: event.target.value
+        });
+    }
+    handleDelete(event) {
+        event.preventDefault();
+        this.setState({
+            title: '',
+            content: ''
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        alert('A form was submitted: ' + this.state.title + ' ' + this.state.content);
+    }
     render() {
         return (
             <div className="container box">
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="field">
                         <label className="label">Titulo</label>
                         <div className="control">
-                            <input className="input" type="text" placeholder="Titulo" />
+                            <input
+                                className="input"
+                                type="text"
+                                placeholder="Titulo"
+                                onChange={this.handleTitleChange}
+                            />
                         </div>
                     </div>
                     <div className="field">
                         <label className="label">Message</label>
                         <div className="control">
-                            <textarea className="textarea" placeholder="Textarea" defaultValue={""} />
+                            <textarea
+                                className="textarea"
+                                placeholder="Textarea"
+                                defaultValue={""}
+                                onChange={this.handleContentChange}
+                            />
                         </div>
                     </div>
                     <div className="field is-grouped">
                         <div className="control">
-                            <button className="button is-link">Submit</button>
+                            <button className="button is-link" type="submit">Submit</button>
                         </div>
                         <div className="control">
-                            <button className="button is-link is-light">Cancel</button>
+                            <button className="button is-link is-light" type="button" onClick={this.handleDelete}>Cancel</button>
                         </div>
                     </div>
                 </form>
