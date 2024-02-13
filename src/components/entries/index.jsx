16503794603af3da1507396aafcbb846ec81fb8f
Ronald Bello <ronaldbello2@gmail.com>
2024-02-13 03:00:04 -0300

    mejorado iconos que desaparecen

--- src/components/entries/index.jsx
+++ src/components/entries/index.jsx
@@ -1,45 +1,7 @@
 import React from 'react';
 import Form from './form';
-import Pog from '../../services/journal';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Table from './table';
 export default class Entries extends React.Component {
-    tableEntries() {
-        let rows = [];
-        console.log(this.props.journalEntries);
-        if (!this.props.journalEntries) return null;
-        this.props.journalEntries.forEach((entry) => {
-            rows.push(
-                <tr key={entry.id}>
-                    <td style={{ width: '80%' }}>
-                        <span className="icon-text">
-                            <span>{entry.title}</span>
-                            <span className="icon" style={{ cursor: 'pointer' }}>
-                                <FontAwesomeIcon icon="fa-solid fa-trash-can" onClick={() => this.deleteEntry(entry.id)} />
-                            </span>
-                        </span>
-                    </td>
-                    <td style={{ width: '20%' }}>{entry.created_at}</td>
-                </tr>
-            );
-        });
-        return (
-            <div className="table-container box">
-                <table className="table bordered is-striped is-hoverable is-fullwidth">
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-    deleteEntry(id) {
-        const x = window.confirm("Do you really want to delete?");
-        if (!x) return;
-        const pog = new Pog();
-        pog.delete(id);
-        this.props.getJournalEntries();
-    }
     render() {
         return (
             <>
@@ -47,7 +9,10 @@ export default class Entries extends React.Component {
                     <Form getJournalEntries={this.props.getJournalEntries} />
                 </div>
                 <div className="column is-full">
-                    <Table journalEntries={this.props.journalEntries} getJournalEntries={this.props.getJournalEntries} />
+                    <Table
+                        journalEntries={this.props.journalEntries}
+                        getJournalEntries={this.props.getJournalEntries}
+                    />
                 </div >
             </>
         )
--- src/components/entries/table/index.jsx
+++ src/components/entries/table/index.jsx
@@ -3,33 +3,33 @@ import Pog from '../../../services/journal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default class Table extends React.Component {
     tableEntries() {
-        let rows = [];
         console.log(this.props.journalEntries);
         if (!this.props.journalEntries) return null;
-        this.props.journalEntries.forEach((entry) => {
-            rows.push(
-                <tr key={entry.id}>
-                    <td style={{ width: '80%' }}>
-                        <span className="icon-text">
-                            <span>{entry.title}</span>
-                            <span
-                                className="icon" style={{ cursor: 'pointer', opacity: .2 }}
-                                onMouseEnter={(e) => e.currentTarget.style.opacity = 1}
-                                onMouseLeave={(e) => e.currentTarget.style.opacity = .2}
-                            >
-                                <FontAwesomeIcon icon="fa-solid fa-trash-can" onClick={() => this.deleteEntry(entry.id)} />
-                            </span>
+        return this.props.journalEntries.map((entry) => (
+            <tr key={entry.id}
+                onMouseEnter={() => this.showIcons(entry.id, 1)}
+                onMouseLeave={() => this.showIcons(entry.id)}
+            >
+                <td style={{ width: '80%' }}>
+                    <span className="icon-text">
+                        <span>{entry.title}</span>
+                        <span id={`delete${entry.id}`}
+                            className="icon"
+                            style={{ cursor: 'pointer', opacity: .0 }}>
+                            <FontAwesomeIcon icon="fa-solid fa-trash-can" onClick={() => this.deleteEntry(entry.id)} />
                         </span>
-                    </td>
-                    <td style={{ width: '20%' }}>{entry.created_at}</td>
-                </tr>
-            );
-        });
-        return rows
+                    </span>
+                </td>
+                <td style={{ width: '20%' }}>{entry.created_at}</td>
+            </tr>
+        ));
+    }
+    showIcons(id, opacity = 0) {
+        document.getElementById(`delete${id}`).style.opacity = opacity;
     }
     deleteEntry(id) {
-        const x = window.confirm("Do you really want to delete?");
-        if (!x) return;
+        const answer = window.confirm("Do you really want to delete?");
+        if (!answer) return;
         const pog = new Pog();
         pog.delete(id);
         this.props.getJournalEntries();
